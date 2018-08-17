cd7ff18300b7d12f66df47af24b52020dffa0102
bieradek <radoslaw.biedrzycki@syncron.com>
2018-08-17 18:58:00 +0200

    One way of accessing methods located inside the other classes

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -7,7 +7,10 @@ public class Main {
         Case theCase = new Case("220B", "Dell", "240", new Dimensions(40, 60, 30));
         Motherboard theMotherboard = new Motherboard("520Z", "Asus", 2, 2, "V2");
         Monitor theMonitor = new Monitor("P25ASZ", "Dell", 25, new Resolution(1920, 1080));
-
         PC pc = new PC(theCase, theMonitor, theMotherboard);
+        // one way of accessing other classes' methods
+        pc.getMonitor().drawPixelAt(50, 20, "blue");
+        pc.getMotherboard().loadProgram("Windows");
+        pc.getTheCase().pressPowerButton();
     }
 }
