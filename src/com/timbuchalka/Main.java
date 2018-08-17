d64af143570b63ffd5a456f77a599122764d4f0c
bieradek <radoslaw.biedrzycki@syncron.com>
2018-08-17 18:43:12 +0200

    Assembling the PC using the previously created classes

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -3,6 +3,11 @@ package com.timbuchalka;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // the goal is to build a PC with all the components, therefore we're first creating objects of the components
+        Case theCase = new Case("220B", "Dell", "240", new Dimensions(40, 60, 30));
+        Motherboard theMotherboard = new Motherboard("520Z", "Asus", 2, 2, "V2");
+        Monitor theMonitor = new Monitor("P25ASZ", "Dell", 25, new Resolution(1920, 1080));
+
+        PC pc = new PC(theCase, theMonitor, theMotherboard);
     }
 }
