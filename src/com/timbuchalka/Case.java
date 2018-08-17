2d8e5b5f66cd438ecea5c902fcdbf6570470ce39
bieradek <radoslaw.biedrzycki@syncron.com>
2018-08-17 18:06:08 +0200

    Some minor addition

--- src/com/timbuchalka/Case.java
+++ src/com/timbuchalka/Case.java
@@ -13,6 +13,10 @@ public class Case {
         this.dimensions = dimensions;
     }
 
+    public void pressPowerButton() {
+        System.out.println("Power button pressed");
+    }
+
     public String getModel() {
         return model;
     }
